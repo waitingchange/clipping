99d9c15d6e7cd088f2e974799e30445e5e6d5838
waitingchange <waiting0313@gmail.com>
2015-10-24 11:42:10 +0800

    aa啊啊啊

--- src/app.js
+++ src/app.js
@@ -7,43 +7,6 @@ var HelloWorldLayer = cc.Layer.extend({
         var size = cc.winSize;
 
 
-        //var bg = new cc.Sprite(res.HelloWorld_png);
-        //bg.setPosition(size.width/2 , size.height/2);
-        //this.addChild(bg);
-
-
-        // var clipper = new cc.ClippingNode();
-        // clipper.tag = 101;
-        // clipper.width = 200;
-        // clipper.height = 200;
-        // clipper.anchorX = 0.5;
-        // clipper.anchorY = 0.5;
-        // clipper.x = this.width / 2;
-        // clipper.y = this.height / 2;
-        // this.addChild(clipper);
-
-        // //clipper.setInverted(true);
-        // clipper.setInverted(false);
-        // var stencil = new cc.DrawNode();
-        // var rectangle = [cc.p(0, 0),cc.p(clipper.width, 0),
-        //     cc.p(clipper.width, clipper.height),
-        //     cc.p(0, clipper.height)];
-        // var white = cc.color(100, 20, 255, 10);
-
-        // stencil.drawPoly(rectangle, white, 1, white);
-        // clipper.stencil = stencil;
-
-        // var content = new cc.Sprite(res.HelloWorld_png);
-        // content.tag = 102;
-        // content.anchorX = 0.5;
-        // content.anchorY = 0.5;
-        // content.x = clipper.width /2 ;
-        // content.y = clipper.height / 2;
-        // clipper.addChild(content);
-
-
-        
-
         var spClose = new cc.Sprite(res.CloseNormal_png);
         spClose.setAnchorPoint(cc.p(0,0));
         spClose.setPosition(0,200);
